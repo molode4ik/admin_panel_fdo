f5fc781d690e197385f4a92f4d7063ed7dcc5cb8
molode4ik <cs1.6l0vemike@gmail.com>
2022-11-16 23:16:25 +0300

    admins update

--- app/exchange_app/views.py
+++ app/exchange_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Teachers, SimpleTable
 from django.contrib.auth import authenticate, login
 from .scripts import check_auth
 from django.contrib.auth.hashers import make_password
